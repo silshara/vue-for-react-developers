81d1b2f45f8d1124ed44e9ea57e956bbaac0c594
Nilushi Silshara <silshara@gmail.com>
2025-02-09 02:58:55 +0200

    send messages  by pressing the enter key in the text input field.

--- src/components/Chat/Compose.vue
+++ src/components/Chat/Compose.vue
@@ -34,7 +34,7 @@ function send() {
       <!-- Hint: See if you could send the message by pressing the enter key in the text field.
                  https://vuejs.org/guide/essentials/event-handling.html#key-modifiers
       -->
-      <Input :value="text" placeholder="Type a message" @change="onChange" />
+      <Input :value="text" placeholder="Type a message" @change="onChange" @keydown.enter="send"/>
 
       <!-- Hint: Call the 'send()' function when the button emits a 'click' event. -->
       <Button icon="send" @click="send"/>
